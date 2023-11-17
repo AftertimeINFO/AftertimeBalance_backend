6cef1b6607658a75e32f6e19461dd658fd9553d3
NeoUKR <81991143+NeoUKR@users.noreply.github.com>
2023-11-17 12:55:30 +0200

    1. Move DB configuration to external file

--- config/settings/general.py
+++ config/settings/general.py
@@ -115,22 +115,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'aftertime',
-        'USER': 'aftertime',
-        'PASSWORD': 'aftertime',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
